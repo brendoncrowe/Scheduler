792972323238551b8df47f530ff67fee4294aacf
Brendon Crowe <brendon.m.crowe@gmail.com>
2022-12-27 14:03:17 -0500

    Initial Commit

---
+++ Scheduler/CreateEvent.swift
@@ -0,0 +1,19 @@
+//
+//  CreateEventViewController.swift
+//  Scheduler
+//
+//  Created by Brendon Crowe on 12/27/22.
+//
+
+import UIKit
+
+class CreateEvent: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
